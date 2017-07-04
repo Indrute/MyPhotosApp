76ebb2ba3530dbc95b57fe9da833d4b653f89c15
Indre <indre@pixelmators-macbook-pro-2.local>
2017-07-04 19:36:42 +0300

    Initial Commit

---
+++ MyPhotosApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyPhotosApp
+//
+//  Created by Indre on 04/07/17.
+//  Copyright © 2017 Indre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
